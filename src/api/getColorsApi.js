79a095ad22f62fb3dcac40ed298b2891b7dbb349
ankitbhurji <bhurji204@gmail.com>
2023-02-11 03:17:38 +0530

    fix promise error

--- src/api/getColorsApi.js
+++ src/api/getColorsApi.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-function getColorsApi() {
+async function getColorsApi() {
     // const url = 'http://localhost:3001/api/storeproducts/colors'
     const url = 'https://dreamy-store-app-backend.onrender.com/api/storeproducts/colors'
-    const getColors = axios.get(url)
+    const getColors = await axios.get(url)
     return getColors
 }
 
