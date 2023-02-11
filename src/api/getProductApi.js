9587450dbcd4bfe19cdb551ab92857777611d52c
ankitbhurji <bhurji204@gmail.com>
2023-02-11 16:13:34 +0530

    update url

--- src/api/getProductApi.js
+++ src/api/getProductApi.js
@@ -5,8 +5,8 @@ async function getProductApi(filters) {
     // const url = 'http://localhost:3001/api/storeproducts/products'
     // const url = `http://localhost:3001/api/storeproducts/products/data?category=${filters.category}&company=${filters.company}&color=${filters.color}&price=${filters.price}`
     // const url = `http://localhost:3001/api/storeproducts/products/data?category=${filters.category}&company=${filters.company}&color=${filters.color}&price=${filters.price}&short=${filters.shortBy}`
-    const url = `http://localhost:3001/api/storeproducts/products/data?category=${filters.category}&company=${filters.company}&color=${filters.color}&price=${filters.price}&short=${filters.shortBy}&shipping=${shipping}`
-    // const url = `https://dreamy-store-app-backend.onrender.com/api/storeproducts/products/data?category=${filters.category}&company=${filters.company}&color=${filters.color}&price=${filters.price}&short=${filters.shortBy}&shipping=${shipping}`
+    // const url = `http://localhost:3001/api/storeproducts/products/data?category=${filters.category}&company=${filters.company}&color=${filters.color}&price=${filters.price}&short=${filters.shortBy}&shipping=${shipping}`
+    const url = `https://dreamy-store-app-backend.onrender.com/api/storeproducts/products/data?category=${filters.category}&company=${filters.company}&color=${filters.color}&price=${filters.price}&short=${filters.shortBy}&shipping=${shipping}`
     const getProducts = await axios.get(url)
     // console.log(typeof(filters.freeshipping.toString()))
     return getProducts;
